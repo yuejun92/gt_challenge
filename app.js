23bf0afc8d3a6d864149285273490aa49ca0c96c
yuejun92 <yue_jun_1@hotmail.com>
2019-08-03 11:23:11 +0800

    Restructure file

--- app.js
+++ app.js
@@ -1,46 +1,5 @@
-const fs = require('fs');
+const controller = require('./controller.js');
 
 const directoryPath = __dirname;
 
-readTODO(directoryPath);
-
-async function readTODO(path){
-  var files = await readDir(path);
-  for(var i = 0; i < files.length; i++){
-    var file = files[i];
-    var currentFilePath = path + '/' + file;
-
-    // Check if current file is a folder
-    if (fs.statSync(currentFilePath).isDirectory()) {
-      // Continue to go in to folder
-      await readTODO(currentFile);
-    }
-    else{
-      // Read current file;
-      var data = await readFile(currentFilePath);
-
-      // Check if file contains "TODO" and ignore "app.js"
-      if(data.toString().includes("TODO") && file !== "app.js"){
-        console.log(currentFile);
-      }
-    }
-  }
-}
-
-function readDir(path){
-  return new Promise((resolve, reject) => {
-    fs.readdir(path, function (err, files) {
-      if(err) reject(err);
-      resolve(files);
-    });
-  });
-}
-
-function readFile(path){
-  return new Promise((resolve, reject) => {
-    fs.readFile(path, function read(err, data) {
-      if(err) reject(err);
-      resolve(data);
-    });
-  })
-}
+controller.readTODO(directoryPath);
